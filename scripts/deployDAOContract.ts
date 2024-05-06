8a66098672ef3b66ee772fa4a0d13a5b2c3497b5
Yarix <yaroslav@chikal.uk>
2024-05-06 16:23:22 +0500

    Deposit test added

--- scripts/deployDAOContract.ts
+++ scripts/deployDAOContract.ts
@@ -1,9 +1,18 @@
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 import { DAOContract } from '../wrappers/DAOContract';
 import { NetworkProvider } from '@ton/blueprint';
 
+
+let workChain = 0; // fill in the workChain
+let hash = Buffer.from(''); // fill in the hash
+let deployer = new Address(workChain, hash);
+
 export async function run(provider: NetworkProvider) {
-    const dAOContract = provider.open(await DAOContract.fromInit());
+    const dAOContract = provider.open(await DAOContract.fromInit({
+        $$type: 'DeployNewDAO',
+        owner: deployer,
+        max_supply: toNano('1000000'),
+    }));
 
     await dAOContract.send(
         provider.sender(),
--- tests/DAOContract.spec.ts
+++ tests/DAOContract.spec.ts
@@ -1,21 +1,29 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { toNano } from '@ton/core';
 import { DAOContract } from '../wrappers/DAOContract';
+import { JettonDefaultWallet } from '../wrappers/JettonDefaultWallet';
 import '@ton/test-utils';
+import { bigint } from 'zod';
 
 describe('DAOContract', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let dAOContract: SandboxContract<DAOContract>;
+    let investor: SandboxContract<TreasuryContract>;
+    let daoContract: SandboxContract<DAOContract>;
+    let wallet: SandboxContract<JettonDefaultWallet>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-
-        dAOContract = blockchain.openContract(await DAOContract.fromInit());
-
         deployer = await blockchain.treasury('deployer');
+        investor = await blockchain.treasury('investor');
 
-        const deployResult = await dAOContract.send(
+        daoContract = blockchain.openContract(await DAOContract.fromInit({
+            $$type: 'DeployNewDAO',
+            owner: deployer.address,
+            max_supply: toNano('1000000'),
+        }));
+
+        const deployResult = await daoContract.send(
             deployer.getSender(),
             {
                 value: toNano('0.05'),
@@ -28,14 +36,65 @@ describe('DAOContract', () => {
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
-            to: dAOContract.address,
+            to: daoContract.address,
             deploy: true,
             success: true,
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and dAOContract are ready to use
+    it('Owner can create wallet for someone', async () => {
+
+        // const mintableBefore = await daoContract.getGetMintable();
+
+        await daoContract.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            "Owner: DepositOpen"
+        );
+
+        // const mintableAfter = await daoContract.getGetMintable();
+
+        // expect(mintableBefore).not.toEqual(mintableAfter);
+
+        const tx = await daoContract.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Deposit',
+                amount: toNano('1337'),
+                receiver: investor.address
+            }
+        );
+
+        expect(tx.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: daoContract.address,
+            success: true,
+        });
+
+        console.log("tx: ", tx);
+
+        tx.events.forEach(event => {
+            if(event.type === 'account_created') {
+                wallet = blockchain.openContract(JettonDefaultWallet.fromAddress(event.account));
+            }
+        });
+
+        // wallet = blockchain.openContract(await JettonDefaultWallet.fromAddress(certAddress));
+
+        const walletData = await wallet.getGetWalletData();
+        // expect(investorBalance).toEqual(toNano('1337'));
+        console.log("investorBalance: ", walletData.balance);
+        console.log("Wallet owner: ", walletData.owner);
+        console.log("investor address: ", investor.address);
+        console.log("toNano('1337'): ", toNano('1337'));
+    });
+
+    it('Someone can create wallet', async () => {
+
     });
 });
